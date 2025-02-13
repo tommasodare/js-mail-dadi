850fc81a311851c144a38b6fdb558543a8189b8e
tommaso.dare <tommaso.dare96@gmail.com>
2025-02-13 14:18:14 +0100

    Done Mail

--- mail/main.js
+++ mail/main.js
@@ -2,3 +2,19 @@
 Crea una lista di email di invitati ad una festa.
 Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo. */
+
+const partyList = ["marco@gmail.com", "nicola@gmail.com", "lucia@gmail.com", "fabrizio@gmail.com"]
+//console.log(partyList);
+const emailUser = prompt("Insert here you email");
+//console.log(emailUser);
+ 
+for (let index = 0; index < partyList.length; index++) {
+    const myPartyList = partyList[index];
+    //console.log(myPartyList);
+
+    if (myPartyList === emailUser) {
+        console.log(`Your email:${emailUser} is in the list`);
+    } else {
+        console.log(`Your email:${emailUser} isn't in the list`);
+    }
+}
