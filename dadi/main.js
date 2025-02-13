8a1bd8ee865f71bd5d7164aecf7cbb1cb7bad749
tommaso.dare <tommaso.dare96@gmail.com>
2025-02-13 16:31:31 +0100

    Done Dice Exercise

--- dadi/main.js
+++ dadi/main.js
@@ -1,4 +1,15 @@
-/* 📌 Gioco dei dadi
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. 
+/* Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. 
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 Prima di partire a scrivere codice poniamoci qualche domanda per individuare gli strumenti necessari allo sviluppo. */
+
+const userNumber = Math.floor(Math.random()*7)
+console.log(userNumber);
+
+const userPc = Math.floor(Math.random()*7)
+console.log(userPc);
+
+if (userPc > userNumber) {
+    console.log("Il pc ha vinto");
+} else {
+    console.log("Il giocatore ha vinto");
+}
--- mail/main.bonus/main.js
+++ mail/main.bonus/main.js
@@ -1,16 +1,16 @@
 /* 👉 Bonus:
-Dopo aver svolto l'esercizio con il for  fai una cartella bonus e svolgilo anche anche usando un metodo delle array tipo includes */
+Dopo aver svolto l'esercizio con il [for] fai una cartella bonus e svolgilo anche anche usando un metodo delle array tipo [includes] */
 
 const partyList = ["marco@gmail.com", "nicola@gmail.com", "lucia@gmail.com", "fabrizio@gmail.com"]
 //console.log(partyList);
 const emailUser = prompt("Insert here you email");
 //console.log(emailUser);
- 
+
 for (let index = 0; index < partyList.length; index++) {
     const myPartyList = partyList[index];
     //console.log(myPartyList);
 
     if (myPartyList.includes(emailUser)) {
-        alert(`Your email:${emailUser} is in the list`); 
+        alert(`Your email:${emailUser} is in the list`);
     }
 }
