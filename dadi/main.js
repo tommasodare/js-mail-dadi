6170804f4fcc675109259fce9cd865ceb4928ba6
tommaso.dare <tommaso.dare96@gmail.com>
2025-02-13 13:39:01 +0100

    first commit

---
+++ dadi/main.js
@@ -0,0 +1,4 @@
+/* 📌 Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. 
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda per individuare gli strumenti necessari allo sviluppo. */
---
+++ mail/main.js
@@ -0,0 +1,4 @@
+/* 📌 Mail
+Crea una lista di email di invitati ad una festa.
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo. */
